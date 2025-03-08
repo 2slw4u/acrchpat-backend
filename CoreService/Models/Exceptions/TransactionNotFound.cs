b71fd5f1aa8a67695e42fe47dd59b9e79af9c856
2slw4u <56259759+2slw4u@users.noreply.github.com>
2025-03-08 13:13:30 +0700

    Merge internal-methods into core-service

---
+++ CoreService/Models/Exceptions/TransactionNotFound.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace CoreService.Models.Exceptions
+{
+    public class TransactionNotFound : ExceptionToResponseProxy
+    {
+        public TransactionNotFound() : base("Искомая транзакция не найдена", HttpStatusCode.NotFound) { }
+        public TransactionNotFound(string message, HttpStatusCode statusCode = HttpStatusCode.NotFound) : base(message) { }
+    }
+}
---
+++ CoreService/Models/Request/Transaction/GetTransactionsDataRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreService.Models.Request.Transaction
+{
+    public class GetTransactionsDataRequest
+    {
+        [Required]
+        [FromQuery]
+        public List<Guid> Transactions { get; set; }
+    }
+}
