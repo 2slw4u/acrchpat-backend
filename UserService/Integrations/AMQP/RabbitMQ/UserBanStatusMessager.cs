3cfaf56f8d6c18b6199bdc2b658d6cd1ac8a8354
Бухарова Виктория <viktoriya.bukharova@gazprombank.ru>
2025-03-09 23:34:49 +0700

    unify producer

---
+++ UserService/Integrations/AMQP/RabbitMQ/UserBanStatusMessager.cs
@@ -0,0 +1,20 @@
+﻿using UserService.Integrations.AMQP.RabbitMQ.Producer;
+using UserService.Models.DTOs;
+
+namespace UserService.Integrations.AMQP.RabbitMQ
+{
+	public class UserBanStatusMessager
+	{
+		private readonly IRabbitMqProducerService _rabbitMqProducerService;
+
+		public UserBanStatusMessager(IRabbitMqProducerService rabbitMqProducerService)
+		{
+			_rabbitMqProducerService = rabbitMqProducerService;
+		}
+
+		public async Task SendMessage(UserBanStatusUpdateDto dto)
+		{
+			await _rabbitMqProducerService.SendMessageToExchange(dto, "user.ban");
+		}
+	}
+}
