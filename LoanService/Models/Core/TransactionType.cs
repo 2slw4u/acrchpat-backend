d57e657666ff7a7e2378d5b12b13fea84b4cd7b9
sasha <sasha.bazdyrev2004@gmail.com>
2025-03-10 00:38:38 +0700

    добавил конвертер в енам

--- LoanService/Models/Core/TransactionType.cs
+++ LoanService/Models/Core/TransactionType.cs
@@ -1,15 +1,12 @@
 ﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
 
 namespace LoanService.Models.General;
-
+[JsonConverter(typeof(JsonStringEnumConverter<TransactionType>))]
 public enum TransactionType
 {
-    [Description("Withdrawal of funds through ATM")]
     Deposit,
-    [Description("Deposit of funds through ATM")]
     Withdrawal,
-    [Description("Money accrued from taking a loan")]
     LoanAccrual,
-    [Description("Payment for a taken loan")]
     LoanPayment
 }
