9f1fdf8eaf7209496e2b1890f64baa0728ac0aa9
sasha <sasha.bazdyrev2004@gmail.com>
2025-04-25 00:24:00 +0700

    added currency

---
+++ LoanService/Models/Core/CurrencyIso.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LoanService.Models.General;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum CurrencyIso
+{
+    RUB,
+    EUR,
+    USD
+}
