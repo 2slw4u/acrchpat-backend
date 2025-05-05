0eb7e64930453f784d6944f1adabb8dc6e91b6a7
Victoria Bukharova <bvv005tkun@gmail.com>
2025-05-05 17:28:00 +0300

    Update AuthorizationMiddleware.cs

--- PreferenceService/Middleware/AuthorizationMiddleware.cs
+++ PreferenceService/Middleware/AuthorizationMiddleware.cs
@@ -10,40 +10,40 @@ public class AuthorizationMiddleware(RequestDelegate next)
     {
         var path = context.Request.Path.Value;
         
-        //if (path == "" || path != null && (path.StartsWith("/swagger") || path.StartsWith("/favicon.ico")))
-        //{
-        //    await next(context);
-        //    return;
-        //}
+        if (path == "" || path != null && (path.StartsWith("/swagger") || path.StartsWith("/favicon.ico")))
+        {
+           await next(context);
+           return;
+        }
         
-        //try
-        //{
-        //    var userRequester = context.RequestServices.GetRequiredService<UserRequester>();
-        //    var userData = await userRequester.GetCurrentUserAsync();
+        try
+        {
+           var userRequester = context.RequestServices.GetRequiredService<UserRequester>();
+           var userData = await userRequester.GetCurrentUserAsync();
             
-        //    context.Items["UserId"] = userData.Id;
-        //    context.Items["Roles"] = userData.Roles;
-        //}
-        //catch (HttpRequestFailedException ex)
-        //{
-        //    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-        //    await context.Response.WriteAsJsonAsync(new ResponseModel
-        //    {
-        //        Status = "Authorization error",
-        //        Message = ex.Message
-        //    });
-        //    return;
-        //}
-        //catch (Exception e)
-        //{
-        //    context.Response.StatusCode = StatusCodes.Status504GatewayTimeout;
-        //    await context.Response.WriteAsJsonAsync(new ResponseModel
-        //    {
-        //        Status = "Authorization service connection error",
-        //        Message = e.Message
-        //    });
-        //    return;
-        //}
+           context.Items["UserId"] = userData.Id;
+           context.Items["Roles"] = userData.Roles;
+        }
+        catch (HttpRequestFailedException ex)
+        {
+           context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+           await context.Response.WriteAsJsonAsync(new ResponseModel
+           {
+               Status = "Authorization error",
+               Message = ex.Message
+           });
+           return;
+        }
+        catch (Exception e)
+        {
+           context.Response.StatusCode = StatusCodes.Status504GatewayTimeout;
+           await context.Response.WriteAsJsonAsync(new ResponseModel
+           {
+               Status = "Authorization service connection error",
+               Message = e.Message
+           });
+           return;
+        }
 
         await next(context);
     }
